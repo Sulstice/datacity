0bda06c95ec3a2dc93a3ddf0260010d066c166ca
Sulstice <sharifsuliman1@gmail.com>
2020-06-20 16:09:47 -0500

    ss/MASTER added people.py

--- places/cities/dallas/police/people/people.py
+++ places/cities/dallas/police/people/people.py
@@ -1,15 +1,21 @@
 
 import pandas as pd
 
+
 if __name__ == '__main__':
 
 
     import os
 
+    dataframes = []
+
     for filename in os.listdir('./'):
         if filename.endswith(".htm"):
-            df = pd.read_html(filename, match='Dallas Police Department')
-            print (df)
+            dataframes.extend(pd.read_html(filename, match='Dallas Police Department'))
             continue
         else:
             continue
+
+    master = pd.concat(dataframes, ignore_index=True)
+
+    print (master)
