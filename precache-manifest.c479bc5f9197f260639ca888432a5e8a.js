e877b0b429c5aea233e16e131579ec7258f00895
lezied <deizel+lezied@gmail.com>
2020-06-21 14:53:50 -0500

    Updates

---
+++ precache-manifest.c479bc5f9197f260639ca888432a5e8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f12206e9305bb36c64881ed297efce",
+    "url": "/datacity/index.html"
+  },
+  {
+    "revision": "548f5667733ffefa76e3",
+    "url": "/datacity/static/css/2.daf4b330.chunk.css"
+  },
+  {
+    "revision": "56835e7cbb37df8ef9ca",
+    "url": "/datacity/static/css/main.83e565eb.chunk.css"
+  },
+  {
+    "revision": "548f5667733ffefa76e3",
+    "url": "/datacity/static/js/2.910d0a4e.chunk.js"
+  },
+  {
+    "revision": "ba325ec3b12b3131a63f9f2cc103626b",
+    "url": "/datacity/static/js/2.910d0a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56835e7cbb37df8ef9ca",
+    "url": "/datacity/static/js/main.9e5c6808.chunk.js"
+  },
+  {
+    "revision": "b2413eecd9cfe5c05ffe",
+    "url": "/datacity/static/js/runtime-main.f960a0ac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sulstice/datacity/precache-manifest.835fb6c78cb008f9cd057a47b99a702d.js"
+  "/datacity/precache-manifest.c479bc5f9197f260639ca888432a5e8a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sulstice/datacity/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/datacity/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
