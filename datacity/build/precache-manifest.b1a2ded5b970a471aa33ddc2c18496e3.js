15efa9e937c7c54dee8993cdeaa5c07989e453cf
Marvin Corro <corro.marvin@gmail.com>
2020-06-21 11:52:41 -0700

    added built files

---
+++ datacity/build/precache-manifest.b1a2ded5b970a471aa33ddc2c18496e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6be9fd523d5f866e1e28b4cc9c1fef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cb7dfdfb9147c207af6",
+    "url": "/static/css/2.daf4b330.chunk.css"
+  },
+  {
+    "revision": "07fb52447405a0d7c047",
+    "url": "/static/css/main.83e565eb.chunk.css"
+  },
+  {
+    "revision": "3cb7dfdfb9147c207af6",
+    "url": "/static/js/2.447cf5f5.chunk.js"
+  },
+  {
+    "revision": "ba325ec3b12b3131a63f9f2cc103626b",
+    "url": "/static/js/2.447cf5f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07fb52447405a0d7c047",
+    "url": "/static/js/main.8009fb61.chunk.js"
+  },
+  {
+    "revision": "480213b6c48dc9bef265",
+    "url": "/static/js/runtime-main.d35bf3c9.js"
+  }
+]);
